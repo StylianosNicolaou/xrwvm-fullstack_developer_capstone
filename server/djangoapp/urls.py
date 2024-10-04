74f09f83358ab70d932dc9d77378542c8d7e85cd
StylianosNicolaou <nicolaou.s24@live.unic.ac.cy>
2024-10-04 20:05:40 +0300

    many changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     # path for logout
     path(route='logout',  view=views.logout_request, name="logout"),
     
+    # path for get_dealerships
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    
     # path for dealer reviews view
 
     # path for add a review view
